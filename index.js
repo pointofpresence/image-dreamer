de995a2ea10515f99678f067bb2882b5793a1a7e
pointofpresence <git@pointofpresence.ru>
2015-07-18 16:36:58 +0600

    Layers ignorer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var keys    = require("./keys.json"),
     input   = process.argv[2],
+    ignore  = process.argv[3] || false,
     counter = 0;
 
 if (!input) {
@@ -7,7 +8,27 @@ if (!input) {
     process.exit();
 }
 
+var trashLayers = [1, 2, 3, 5, 7, 8, 9, 10, 11, 15, 16, 20, 21, 22, 23, 26, 33, 34, 35, 36, 45, 46, 47, 48, 57, 58, 59, 60, 69, 70, 71, 72, 81, 82, 83, 84, 94, 95, 96, 97, 106, 107, 108, 109];
+
 dreamify();
+
+function inArray(needle, haystack, strict) {
+    var found = false;
+
+    strict = !!strict;
+
+    for (var i = 0; i < haystack.length; i++) {
+        if (
+            (strict && haystack[i] === needle) ||
+            (!strict && haystack[i] == needle)
+        ) {
+            found = true;
+            break;
+        }
+    }
+
+    return found;
+}
 
 function CmdExec(cmd, args, cbEnd) {
     var spawn = require("child_process").spawn,
@@ -38,8 +59,14 @@ function dreamify() {
 
     counter++;
 
-    var key = keys.shift(),
-        name = input + "_" + counter + "_" + key.replace("/", "-") + ".png";
+    var key = keys.shift();
 
+    if (ignore && inArray(counter, trashLayers)) {
+        console.log("\033[36m" + counter + " ignored\n\033[0m");
+        dreamify();
+        return;
+    }
+
+    var name = input + "_" + counter + "_" + key.replace("/", "-") + ".png";
     new CmdExec("ipython", ["dreamify.py", input, name, key], dreamify);
 }
